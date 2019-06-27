05761d387dfd4f123fc405dc59ffaa0dc02a8c62
오경환 <32234263+rudghks373@users.noreply.github.com>
2019-06-27 09:51:07 +0900

    k
    kk

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `test`,
+    description: `테스트 디크립션`,
+    author: `경환쓰`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
